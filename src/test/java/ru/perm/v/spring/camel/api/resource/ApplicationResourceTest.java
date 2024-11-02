7e0cec419abf3ca7fb5b05a99c4c5ddeb4476c6e
Черепахин Василий <vasi.che@gmail.com>
2024-11-02 18:13:48 +0500

    add ApplicationResourceTest

---
+++ src/test/java/ru/perm/v/spring/camel/api/resource/ApplicationResourceTest.java
@@ -0,0 +1,27 @@
+package ru.perm.v.spring.camel.api.resource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import ru.perm.v.spring.camel.api.dto.OrderDTO;
+import ru.perm.v.spring.camel.api.service.OrderService;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@EnableAutoConfiguration // use props from application main/resource/application.properties. If need test param - create test/resource/application.properties
+@SpringBootTest
+class ApplicationResourceTest {
+
+    @Autowired
+    OrderService orderService;
+
+    @Test
+    void getOrders() {
+        List<OrderDTO> orders = orderService.getOrders();
+
+        assertEquals(4, orders.size());
+    }
+}
