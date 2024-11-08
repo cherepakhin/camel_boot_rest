2f9b964064e1086ef129c7c7fdb395a72da43182
Черепахин Василий <vasi.che@gmail.com>
2024-11-08 20:47:16 +0500

    add test ValidateOrderDtoTest.errorForShortNameOrder()

--- src/main/java/ru/perm/v/spring/camel/api/dto/OrderDTO.java
+++ src/main/java/ru/perm/v/spring/camel/api/dto/OrderDTO.java
@@ -17,6 +17,7 @@ public class OrderDTO {
 	
 	private Integer id;
 	@NotNull(message = "Name may not be null")
+	@Size(min = 5, message = "The name must be longer than 5 characters")
 	private String name;
 	@Min(value = 1, message = "Price must be higher than 1")
 	private Number price;
--- src/test/java/ru/perm/v/spring/camel/api/service/impl/ValidateOrderDtoTest.java
+++ src/test/java/ru/perm/v/spring/camel/api/service/impl/ValidateOrderDtoTest.java
@@ -17,7 +17,7 @@ public class ValidateOrderDtoTest {
     @Test
     void forNegativePrice() {
         Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
-        OrderDTO dto = new OrderDTO(1, "----", -1F);
+        OrderDTO dto = new OrderDTO(1, "NAME1234", -100);
 
         Set<ConstraintViolation<OrderDTO>> violations = validator.validate(dto);
 
@@ -27,5 +27,19 @@ public class ValidateOrderDtoTest {
         List<ConstraintViolation<OrderDTO>> listViolations = new ArrayList<ConstraintViolation<OrderDTO>>(violations);
         assertEquals(1, listViolations.size());
         assertEquals("Price must be higher than 1", listViolations.get(0).getMessage());
+    }
+    @Test
+    void errorForShortNameOrder() {
+        Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+        OrderDTO dto = new OrderDTO(1, "1234", 100);
+
+        Set<ConstraintViolation<OrderDTO>> violations = validator.validate(dto);
+
+        assertFalse(violations.isEmpty());
+        assertEquals(1, violations.size());
+
+        List<ConstraintViolation<OrderDTO>> listViolations = new ArrayList<ConstraintViolation<OrderDTO>>(violations);
+        assertEquals(1, listViolations.size());
+        assertEquals("The name must be longer than 5 characters", listViolations.get(0).getMessage());
     }
 }
